05d035978bd16a14579545e40fc68e647f1706ff
yoryet <y.danoun@pharmatender.cl>
2024-10-28 23:46:39 -0300

    add package test

---
+++ src/test/java/com/sumativafs3/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumativafs3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
